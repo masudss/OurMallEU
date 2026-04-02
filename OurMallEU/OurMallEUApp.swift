0688e236be347f9cadfcd374de312b439515e552
Onikeku mas'ud <masudonikeku@gmail.com>
2026-04-02 22:33:56 +0100

    Initial Commit

---
+++ OurMallEU/OurMallEUApp.swift
@@ -0,0 +1,17 @@
+//
+//  OurMallEUApp.swift
+//  OurMallEU
+//
+//  Created by Masud on 02/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct OurMallEUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
